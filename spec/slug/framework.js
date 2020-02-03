0784f38d4cb3f58561cb54595b9064cf66896967
cpcwood <cpcwood@outlook.com>
2020-02-03 17:20:54 +0000

    Slug testing framework working

--- spec/slug/framework.js
+++ spec/slug/framework.js
@@ -1,15 +1,14 @@
-function describe(str, funck) {
-  var resultHash = funck()
-  var result = resultHash["passed"]
+function describe(str, resultHash) {
+  var result = resultHash['passed']
   var color = (result == true ? "green" : "red")
   var resultHtml = `
-  <div class = 'result' style = 'color:${color};'> 
-  ${str}
+  <div class = 'result' style = 'color:${color};'>
+  <h3>${str}</h3>
   <ul>
-    <li>result : ${result}</li>
-    <li>message : ${resultHash['message']}</li>
-    <li>expected : ${resultHash['expected']}</li>
-    <li>got : ${resultHash['got']}</li>
+    <li>Result: ${result}</li>
+    <li>Message: ${resultHash['message']}</li>
+    <li>Expected: ${resultHash['expected']}</li>
+    <li>Got: ${resultHash['got']}</li>
     </ul>
   </div> `
 
@@ -19,32 +18,25 @@ function describe(str, funck) {
 function expect(methodValue){
   this.toEq = function(comparisonValue){
     if(comparisonValue === methodValue){
-      return {message: "Passed", passed: true, expected: comparisonValue, got: methodValue}
+      return {message: "Passed", 'passed': true, 'expected': comparisonValue, 'got': methodValue}
     } else {
-      return {message: `Failed. Expected ${comparisonValue}, got ${methodValue}`, passed: false, expected: comparisonValue, got: methodValue}
+      return {message: `Failed. Expected ${comparisonValue}, got ${methodValue}`, 'passed': false, 'expected': comparisonValue, 'got': methodValue}
     }
   };
   this.contains = function(comparisonValue){
     if(methodValue.includes(comparisonValue)) {
-      return {message: "Passed", passed: true, expected: comparisonValue, got: methodValue}
+      return {message: "Passed", 'passed': true, 'expected': comparisonValue, 'got': methodValue}
     }
     else {
-      return {message: `Failed. Expected ${comparisonValue} to be contained within ${methodValue}`, passed: false, expected: comparisonValue, got: methodValue}
+      return {message: `Failed. Expected ${comparisonValue} to be contained within ${methodValue}`, 'passed': false, 'expected': comparisonValue, 'got': methodValue}
     }
   }
  this.toBeA = function(comparisonValue){
     if(typeof methodValue == typeof comparisonValue ) {
-      return {message: "Passed", passed: true, expected: comparisonValue, got: methodValue}
+      return {message: "Passed", 'passed': true, 'expected': comparisonValue, 'got': methodValue}
     } else{
-      return {message: `Failed. Expected ${comparisonValue} to be the same type as ${methodValue}`, passed: false, expected: comparisonValue, got: methodValue}
+      return {message: `Failed. Expected ${comparisonValue} to be the same type as ${methodValue}`, 'passed': false, 'expected': comparisonValue, 'got': methodValue}
     }
   }
   return this
 }
-
-// console.log(expect("hello").toEq("hello"))
-// console.log(expect("hello").toEq("hi"))
-// console.log(expect(["hello", "hi", "bye"]).contains("hello"))
-// console.log(expect(["hi", "bye"]).contains("hello"))
-// console.log(expect(2).toBeA("hello"))
-// console.log(expect("hi").toBeA("hello"))
