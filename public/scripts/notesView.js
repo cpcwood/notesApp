e35ca7bd2507f27fb75e0737e7f337eabc868df1
cpcwood <cpcwood@outlook.com>
2020-02-04 15:44:03 +0000

    template view

--- public/scripts/notesView.js
+++ public/scripts/notesView.js
@@ -1,3 +1,40 @@
+import { forEach } from "async"
+
 function notesView(){
+
+
+  function: renderStructure(){
+    body = get by id or something
+    body.append(
+      `
+      <header> 
+        some header stuff
+      </header>
+
+      <div class='container'>
+
+        <div class='notesList'></div>
+
+        <div class='addNotes'></div>
+      
+      </div>
+      
+      
+      
+      
+      `
+    )
+  }
   
+  function: renderNotesList(notes) {
+    notesContainer = document.querySelector('.notesList')
+    notes.forEach(function(note){
+      notesContainer.append(
+        `
+        <div> notes information interpolated
+        `
+      )
+    })
+
+  }
 }
