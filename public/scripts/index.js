276d642705aaf8230a94ae2cfb6772adfe10fd7e
Student <katierodgers95@gmail.com>
2020-02-05 16:17:35 +0000

    page rendering on load

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,3 +4,4 @@ window.addEventListener('load', function() {
 	// Creates new controller when webpage is opened
 	window.notesController = new NotesController(new NotesModel(), new NotesView());
 });
+
--- public/scripts/notesController.js
+++ public/scripts/notesController.js
@@ -1,3 +1,17 @@
-function notesController(notesModel, notesView) {
+function NotesController(notesModel, notesView) {
+
+  this.notesView = notesView
+  this.notesModel = notesModel
   
+  this.pageSetup = function() {
+    this.notesView.renderStructure();
+    this.notesView.renderNewNote();
+  }
+
+
+
+  // these functions run when controller is created
+  this.pageSetup();
 }
+
+
