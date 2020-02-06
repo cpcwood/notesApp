5b03340710ab681bad2456756c5dc6fab9b407b4
benauld <ben@portobello-consultancy.co.uk>
2020-02-06 15:07:23 +0000

    HTML changes

--- public/scripts/notesController.js
+++ public/scripts/notesController.js
@@ -1,31 +1,25 @@
 function NotesController(notesModel, notesView) {
+	this.notesView = notesView;
+	this.notesModel = notesModel;
 
-  this.notesView = notesView
-  this.notesModel = notesModel
-  
-  this.pageSetup = function() {
-    this.notesView.renderStructure();
-    this.notesView.renderNewNote();
-    var notes = this.notesModel.notesArray;
-    this.notesView.renderNotesList(notes);
-  }
+	this.pageSetup = function() {
+		this.notesView.renderStructure();
+		this.notesView.renderNewNote();
+		var notes = this.notesModel.notesArray;
+		this.notesView.renderNotesList(notes);
+	};
 
-  self = this;
+	self = this;
 
-  
-  this.addingNoteToList = function() {
-    document.getElementById('newMessage').addEventListener('click', function(){
-      var text = self.notesView.extractText();
-      self.notesModel.addNote(text);
-      self.notesView.renderNotesList(self.notesModel.notesArray);
-    });
-    
-  }
+	this.addingNoteToList = function() {
+		document.getElementById('newMessage').addEventListener('click', function() {
+			var text = self.notesView.extractText();
+			self.notesModel.addNote(text);
+			self.notesView.renderNotesList(self.notesModel.notesArray);
+		});
+	};
 
-  
-  // these functions run when controller is created
-  this.pageSetup();
-  this.addingNoteToList();
+	// these functions run when controller is created
+	this.pageSetup();
+	this.addingNoteToList();
 }
-
-
